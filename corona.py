3925041743c5d18286d057d55f8a6fb79b39aaa0
linluk <lukas42singer@gmail.com>
2020-03-23 21:23:38 +0100

    refactored r^2 calculation to ba a distinct function

--- corona.py
+++ corona.py
@@ -63,6 +63,12 @@ def growth_rate(series):
             output.append(d2 / d1)
     return output
 
+def r_square(func, data):
+    y_ = sum(y for y in data) / len(data)
+    SS_tot = sum((data[i] - y_) ** 2 for i in range(len(data)))
+    SS_res = sum((data[i] - func(i)) ** 2 for i in range(len(data)))
+    return 1 - SS_res / SS_tot
+
 
 def main():
     confirmed, recovered, deaths = download_data()
@@ -134,10 +140,7 @@ def main():
     ffit = poly.Polynomial(coefs)
     plt.figure()
     plt.plot(actual, label='Data', color='b', linestyle='', marker='o')
-    y_ = sum(y for y in actual) / len(actual)
-    SS_tot = sum((actual[i] -y_) ** 2 for i in range(len(actual)))
-    SS_res = sum((actual[i] - ffit(i)) ** 2 for i in range(len(actual)))
-    r2 = 1 - SS_res / SS_tot
+    r2 = r_square(ffit, actual)
     plt.plot([ffit(x) for x in range(int(len(actual) * 1.3))], label=f'Fitted  R^2: {r2:.3}', color='g')
     plt.legend()
     sfunc = 'f(x) = ' + ' + '.join(f'{coefs[i]:.3f} * x ^ {i + 1}' for i in range(len(coefs)))
